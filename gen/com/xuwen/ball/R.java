ae34b086e315f033d6187212237e958807916c23
vancecute <18202795239@163.com>
2014-10-24 22:48:33 +0800

    董启凡

--- gen/com/xuwen/ball/R.java
+++ gen/com/xuwen/ball/R.java
@@ -104,19 +104,20 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int white_point=0x7f020045;
     }
     public static final class id {
-        public static final int Logo=0x7f090009;
+        public static final int Logo=0x7f09000a;
         public static final int Logo1=0x7f090002;
-        public static final int action_settings=0x7f09000c;
+        public static final int action_settings=0x7f09000d;
         public static final int container=0x7f090000;
-        public static final int game_control=0x7f090007;
+        public static final int game_control=0x7f090008;
         public static final int gameover=0x7f090001;
-        public static final int geme_canvas=0x7f090006;
-        public static final int help=0x7f090008;
-        public static final int play=0x7f090005;
-        public static final int rank=0x7f09000b;
-        public static final int restart=0x7f090003;
-        public static final int share=0x7f090004;
-        public static final int start=0x7f09000a;
+        public static final int geme_canvas=0x7f090007;
+        public static final int help=0x7f090009;
+        public static final int play=0x7f090006;
+        public static final int rank=0x7f09000c;
+        public static final int restart=0x7f090004;
+        public static final int score=0x7f090003;
+        public static final int share=0x7f090005;
+        public static final int start=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
